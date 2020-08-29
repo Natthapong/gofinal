75b1a239b4d93547fef6217072d1ce4e0cd23042
k0963584 <natthapong.ch@kbtg.tech>
2020-08-29 16:54:40 +0700

    refactor auth to package

--- main.go
+++ main.go
@@ -2,26 +2,16 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 
 	"github.com/Natthapong/gofinal/customer"
+	"github.com/Natthapong/gofinal/middleware"
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
 )
 
-func auth(c *gin.Context) {
-	authKey := c.GetHeader("Authorization")
-	if authKey != "November 10, 2009" {
-		c.JSON(http.StatusUnauthorized, http.StatusText(http.StatusUnauthorized))
-		c.Abort()
-		return
-	}
-	c.Next()
-}
-
 func setupRouter() *gin.Engine {
 	r := gin.Default()
-	r.Use(auth)
+	r.Use(middleware.Auth)
 	r.POST("/customers", customer.CreateCustomerHandler)
 	r.GET("/customers/:id", customer.FindOneCustomerHandler)
 	r.GET("/customers", customer.FindAllCustomerHandler)
