f2ee828af8faaa56644f70298f0d827878cee4b7
k0963584 <natthapong.ch@kbtg.tech>
2020-08-30 10:19:40 +0700

    refactor setup router to api package

---
+++ api/api.go
@@ -0,0 +1,17 @@
+package api
+
+import (
+	"github.com/Natthapong/gofinal/customer"
+	"github.com/Natthapong/gofinal/middleware"
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRouter(r *gin.Engine, h customer.Handler) *gin.Engine {
+	r.Use(middleware.Auth)
+	r.POST("/customers", h.CreateCustomerHandler)
+	r.GET("/customers/:id", h.FindOneCustomerHandler)
+	r.GET("/customers", h.FindAllCustomerHandler)
+	r.PUT("/customers/:id", h.UpdateCustomerHandler)
+	r.DELETE("/customers/:id", h.DeleteCustomerHandler)
+	return r
+}
--- main.go
+++ main.go
@@ -5,22 +5,12 @@ import (
 	"log"
 	"os"
 
+	"github.com/Natthapong/gofinal/api"
 	"github.com/Natthapong/gofinal/customer"
 	"github.com/Natthapong/gofinal/database"
-	"github.com/Natthapong/gofinal/middleware"
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
 )
-
-func setupRouter(r *gin.Engine, h customer.Handler) *gin.Engine {
-	r.Use(middleware.Auth)
-	r.POST("/customers", h.CreateCustomerHandler)
-	r.GET("/customers/:id", h.FindOneCustomerHandler)
-	r.GET("/customers", h.FindAllCustomerHandler)
-	r.PUT("/customers/:id", h.UpdateCustomerHandler)
-	r.DELETE("/customers/:id", h.DeleteCustomerHandler)
-	return r
-}
 
 func main() {
 	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
@@ -32,6 +22,6 @@ func main() {
 	h := customer.Handler{DB: db}
 	r := gin.Default()
 
-	r = setupRouter(r, h)
+	r = api.SetupRouter(r, h)
 	r.Run(":2009")
 }
